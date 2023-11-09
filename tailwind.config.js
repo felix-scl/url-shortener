73264b6312ef2c19e5e72ff37a834480112fb38b
FLX-X <fhernan@live.cl>
2023-11-09 14:48:58 -0300

    add assets/green-gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'green-gradient': "url('./assets/green-gradient.png')",
+      },
+    },
   },
   plugins: [],
 };
