07f09e767d9f7449a0ad57a3299ab35857b5c9eb
FLX-X <fhernan@live.cl>
2023-11-09 15:28:50 -0300

    add main fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        bungee: ['Bungee', 'sans-serif'],
+      },
       backgroundImage: {
         'green-gradient': "url('./assets/green-gradient.png')",
       },
