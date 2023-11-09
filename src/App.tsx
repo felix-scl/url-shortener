d87c3321e6ff247c565617aec7b7b4220a7d013d
FLX-X <fhernan@live.cl>
2023-11-09 14:25:59 -0300

    add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import Header from './components/Header';
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      {/* MAIN CONTENT */}
+      {/* FOOTER */}
+    </>
+  );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header>
+      <a href='/'>URLSh...</a>
+      {/* NAVBAR */}
+    </header>
+  );
+}
