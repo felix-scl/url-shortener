b1787b14e33917a7e37db878aedec0042462a0e5
FLX-X <fhernan@live.cl>
2023-11-25 17:46:45 -0300

    add h1 to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,11 @@ export default function App() {
   return (
     <>
       <Header />
+      <div className='mt-6'>
+        <h1 className='text-center text-3xl text-white font-bungee'>
+          Yet another URL Shortener...
+        </h1>
+      </div>
       <section className='mx-4 mt-10 space-y-10'>
         <ShortenURLForm inputRef={inputRef} />
         <MyURLs focusInput={focusInput} />
